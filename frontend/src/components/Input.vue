3e62251b42f324a0d2151123f469b95ae7f7b384
Kaian <kaianfsantos@gmail.com>
2020-06-03 22:30:17 -0300

    frontend | Ajustes no layout das telas e nos inputs

--- frontend/src/components/Input.vue
+++ frontend/src/components/Input.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="form-group">
     <div v-if="Type === 'text'" class="wrapper">
-      <input type="text" class="input-value" id="text" v-model="value"/>
+      <input type="text" class="input-value" v-model="value"/>
       <label class="input-span" for="email" :class="{ 'active': value }">{{ label }}</label>
       <div class="line"></div>
     </div>
     <div v-if="Type === 'email'" class="wrapper">
-      <input type="email" class="input-value" id="email" v-model="value"/>
+      <input type="email" class="input-value" v-model="value"/>
       <label class="input-span" for="email" :class="{ 'active': value }">{{ label }}</label>
       <div class="line"></div>
     </div>
     <div v-if="Type === 'password'" class="wrapper">
-      <input type="password" class="input-value" id="password" v-model="value"/>
+      <input type="password" class="input-value" v-model="value"/>
       <label class="input-span" for="password" :class="{ 'active': value }">{{ label }}</label>
       <div class="line"></div>
     </div>
@@ -39,6 +39,12 @@ export default {
   .wrapper {
     display: flex;
     flex-direction: column;
-
+    z-index: 3;
+  }
+  .input-value {
+    z-index: 2;
+  }
+  .input-span {
+    z-index: 0;
   }
 </style>
